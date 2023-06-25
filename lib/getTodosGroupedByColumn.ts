0c50cd803b0d864e83b5908638d63e16b1d5d782
Noel Ohaeri <noelohaeri@gmail.com>
2023-06-25 17:02:43 -0400

    Dragging logic

---
+++ lib/getTodosGroupedByColumn.ts
@@ -0,0 +1,54 @@
+import { databases } from "@/appwrite";
+
+export const getTodosGroupedByColumn = async () => {
+  const data = await databases.listDocuments(
+    process.env.NEXT_PUBLIC_DATABASE_ID!,
+    process.env.NEXT_PUBLIC_TODOs_COLLECTION_ID!
+  );
+  const todos = data.documents;
+
+  const columns = todos.reduce((acc, todo) => {
+    if (!acc.get(todo.status)) {
+      acc.set(todo.status, {
+        id: todo.status,
+        todos: [],
+      });
+    }
+
+    acc.get(todo.status)!.todos.push({
+      $id: todo.$id,
+      $createdAt: todo.$createdAt,
+      title: todo.title,
+      status: todo.status,
+      // If there is an image
+      ...(todo.image && { image: JSON.parse(todo.image) }),
+    });
+
+    return acc;
+  }, new Map<TypedColumn, Column>());
+  //   return data;
+
+  const columnTypes: TypedColumn[] = ["todo", "inprogress", "done"];
+  columnTypes.forEach((column) => {
+    if (!columns.get(column)) {
+      columns.set(column, {
+        id: column,
+        todos: [],
+      });
+    }
+  });
+
+  // sorting columns
+
+  const sortedColumns = new Map(
+    Array.from(columns.entries()).sort((a, b) => {
+      return columnTypes.indexOf(a[0]) - columnTypes.indexOf(b[0]);
+    })
+  );
+
+  const board: Board = {
+    columns: sortedColumns,
+  };
+
+  return board;
+};
